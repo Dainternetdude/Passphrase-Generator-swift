e56a87e0ff71fc5291c3046d43b37c7958e0bce5
Dainternetdude <randomdude@kmamedia.com>
2022-04-22 13:14:20 -0600

    Initial Commit

---
+++ Passphrase Generator/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Passphrase Generator
+//
+//  Created by Dainternetdude on 2022-04-22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
