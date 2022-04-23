9c67e4285a04ad9632d83ea41ffad47246243316
Dainternetdude <randomdude@kmamedia.com>
2022-04-22 18:52:59 -0600

    added gui

--- Passphrase Generator/AppDelegate.swift
+++ Passphrase Generator/AppDelegate.swift
@@ -10,11 +10,14 @@ import Cocoa
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-    
+    let statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
 
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
+		statusItem.button?.title = "🅿"
+		statusItem.button?.target = self
+		statusItem.button?.action = #selector(showSettings)
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
@@ -25,6 +28,16 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         return true
     }
 
-
+	@objc func showSettings() {
+		let storyBoard = NSStoryboard(name: "Main", bundle: nil)
+		guard let vc = storyBoard.instantiateController(withIdentifier: "ViewController") as? ViewController else {
+			fatalError("Unable to find ViewController in the storyboard")
+		}
+		
+		let popoverView = NSPopover()
+		popoverView.contentViewController = vc
+		popoverView.behavior = .transient
+		popoverView.show(relativeTo: statusItem.button!.bounds, of: statusItem.button!, preferredEdge: .maxY)
+	}
 }
 
